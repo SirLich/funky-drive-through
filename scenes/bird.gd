3a7aac2a8b0af4b6a7a0df452838412d13e7089a
SirLich <lrkoehler77@gmail.com>
2024-01-27 10:55:09 +0100

    Add neutral animation

--- scenes/bird.gd
+++ scenes/bird.gd
@@ -1,7 +1,23 @@
 extends Node2D
 
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
+
+func become_angry():
+	animation_player.play("angry")
+	animation_player.animation_finished.connect(on_animation_finished)
+
+func become_neutral():
+	animation_player.play("neutral")
+
+func become_happy():
+	animation_player.play("happy")
+	animation_player.animation_finished.connect(on_animation_finished)
+	
+func on_animation_finished():
+	become_neutral()
+	
 func _ready() -> void:
-	pass # Replace with function body.
+	become_neutral()
 
 func _process(delta: float) -> void:
 	pass
